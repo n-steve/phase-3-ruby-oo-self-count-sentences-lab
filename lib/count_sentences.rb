afd7a7532c657ace780c47b55ae97efc32c1b401
steve <steve.nguyen2013@gmail.com>
2022-07-22 14:08:35 -0700

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self.include?(".") ? true : false
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    # self.include?("?") ? true : false
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    # self.include?("!") ? true : false
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+  # return self.split(" ").count
+ 
+ self.split(/\.|\?|!/).reject(&:empty?).length
+
 
   end
 end
